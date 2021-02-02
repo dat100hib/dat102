41289ec2f7079983a30b967533a6714559b123d9
torje <torje@192.168.10.138>
2021-02-02 11:33:21 +0100

    Endringer

--- Kalkulator/Test/no/hvl/dat102/TestKalkulator.java
+++ Kalkulator/Test/no/hvl/dat102/TestKalkulator.java
@@ -1,51 +1,7 @@
-<<<<<<< HEAD
 package no.hvl.dat102;
-
-import static org.junit.Assert.*;
 
 import org.junit.Test;
-
-import no.hvl.dat102.Kalkulator;
-
-public class TestKalkulator {
-	//testmetoder
-
-	@Test
-	public void testAdd() {
-		assertEquals("Test av 1+5", 6, Kalkulator.add(1, 5));
-	}
-	
-	
-	@Test
-	public void testT() {
-		assertFalse(Kalkulator.t());
-	}
-
-	@Test
-	public void testSub() {
-		assertEquals("Test av 4-2", 2, Kalkulator.sub(4, 2));
-		assertEquals("Test av 2-4", -2, Kalkulator.sub(2, 4));
-
-	}
-
-	@Test
-	public void testMul() {
-		assertEquals("Test av 3*2", 6, Kalkulator.mul(3, 2));
-	}
-
-	@Test
-	public void testDiv() {
-		assertEquals("Test av 4/2", 2, Kalkulator.div(4, 2));
-	}
-
-}
-=======
-package no.hvl.dat102;
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
 
 public class TestKalkulator {
 	//testmetoder
@@ -73,4 +29,4 @@ public class TestKalkulator {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/587851/dat102.git
+
--- Kalkulator/src/no/hvl/dat102/Kalkulator.java
+++ Kalkulator/src/no/hvl/dat102/Kalkulator.java
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-package no.hvl.dat102;
 
-public class Kalkulator {
-
-	public static int add(int tall1, int tall2) {
-		return tall1 + tall2;
-	}
-
-	public static int sub(int tall1, int tall2) {
-		return tall1 - tall2;
-	}
-	
-	
-	public static int mul(int tall1, int tall2) {
-		return tall1 * tall2;
-	}
-
-	public static int div(int tall1, int tall2) {
-		return tall1 / tall2;
-	}
-	
-	public static boolean t() {
-		return false;
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-
-	}
-
-}
-=======
 package no.hvl.dat102;
 
 public class Kalkulator {
@@ -66,4 +30,4 @@ public class Kalkulator {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/587851/dat102.git
+
